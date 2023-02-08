16829881871a8af77e89f1462b6d559677d605f3
idabblewith <109533655+idabblewith@users.noreply.github.com>
2023-02-08 18:09:17 +0800

    Bill Splitter
    Added Day 2

--- tools/days/day_002/main.py
+++ tools/days/day_002/main.py
@@ -2,3 +2,12 @@ from days.day_002.files.helpers import *
 
 def day_002():
 	title("BILL SPLITTER")
+	# Numbers appropriately cast for each variable
+	bill = float(nli("What was your total bill?"))
+	tip_percentage = int(nli("What percentage would you like to tip?\n0, 10, 12, or 15?"))
+	amount_of_people = int(nli("How many people are splitting the bill?"))
+	# Adds the tip to the bill
+	total_bill = (bill * (tip_percentage/100)) + bill 
+	# Sets the payment pp as a 2 decimal floating number
+	payment_per_person = "{:.2f}".format(total_bill/amount_of_people)
+	nls(f'Each person should pay: ${payment_per_person}')

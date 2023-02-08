16b140b15a1f38b927775eb7b7f71624f0e07858
idabblewith <109533655+idabblewith@users.noreply.github.com>
2023-02-08 17:42:18 +0800

    Band Name Generator
    Day 1 Added

--- tools/days/day_001/main.py
+++ tools/days/day_001/main.py
@@ -2,3 +2,6 @@ from days.day_001.files.helpers import *
 
 def day_001():
 	title("BAND NAME GENERATOR")
+	city = nli("What's the name of the city you grew up in?")
+	pet = nli("What's your pet's name?")
+	nls(f"Your band name is: {pet} {city}.")

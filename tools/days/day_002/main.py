3abd108f01eb941f6f45670d73ca8882073728fb
idabblewith <109533655+idabblewith@users.noreply.github.com>
2023-02-08 18:17:36 +0800

    Update main.py

--- tools/days/day_002/main.py
+++ tools/days/day_002/main.py
@@ -8,6 +8,6 @@ def day_002():
 	amount_of_people = int(nli("How many people are splitting the bill?"))
 	# Adds the tip to the bill
 	total_bill = (bill * (tip_percentage/100)) + bill 
-	# Sets the payment pp as a 2 decimal floating number
+	# Sets the payment pp as a 2 decimal floating number (as string)
 	payment_per_person = "{:.2f}".format(total_bill/amount_of_people)
 	nls(f'Each person should pay: ${payment_per_person}')
